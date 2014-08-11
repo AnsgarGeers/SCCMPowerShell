69539c6f246c04e79b800ea27bf68358cd761fe8
Joshua Duffney <duffney@outlook.com>
2014-08-11 08:49:15 -0500

    Added Parameters and a Function
    Made the script easier to use with a function and parameters.

--- CreateCatalogCategories.ps1
+++ CreateCatalogCategories.ps1
@@ -6,18 +6,47 @@
 # Comments: Populate the names of the desired catalog categories line by line in catalogcategories.txt
 # ---------------------------------------------------
 
-$File = "c:\scripts\catalogcategories.txt"
+Function NewCMCatalogCategories {
 
-ForEach ($Category in (Get-Content $File))
-{
-    $CategoryTest =  Get-CMCategory -Name "$Category"
-    
-    if ($CategoryTest -eq $null){
-    New-CMCategory -CategoryType CatalogCategories -Name $Category | Out-Null
-    Write-Host -ForegroundColor Green "$Category Created"
-    } 
-    else 
+Param(
+    [string]$SiteServerName,
+    [string]$SiteCode,
+    [string]$Path
+)
+# Connect to SCCM
+    Try
     {
-    Write-Host -ForegroundColor red "$Category already exists"
+    Import-Module "$(Split-Path $env:SMS_ADMIN_UI_PATH -Parent)\ConfigurationManager.psd1" -ErrorAction Stop
+    Set-Location "$($SiteCode):"
     }
+    Catch [System.IO.FileNotFoundException]
+    {
+        "SCCM Admin Console not installed"
+    }
+    Catch
+    {
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName
+    }
+    Finally
+    {
+        "This Script attempted to import the SCCM module"
+    }
+# Create Catalog Catagories
+    ForEach ($Category in (Get-Content $Path))
+    {
+        $CategoryTest =  Get-CMCategory -Name "$Category"
+    
+        if ($CategoryTest -eq $null){
+        New-CMCategory -CategoryType CatalogCategories -Name $Category | Out-Null
+        Write-Host -ForegroundColor Green "$Category Created"
+        } 
+        else 
+        {
+        Write-Host -ForegroundColor red "$Category already exists"
+        }
+    }
+
 }
+
+NewCMCatalogCategories -SiteServerName ServerName -SiteCode SiteCode -Path "C:\scripts\catalogcategories.txt"
